99501826509e65cb4c559c9cf4f77d67aaea5dd8
t2023-m0072 <t2023-m0072@t2023-m0072ui-MacBookAir.local>
2024-11-19 20:07:51 +0900

    initializing seatpage.dart

--- lib/pages/seatpage.dart
+++ lib/pages/seatpage.dart
@@ -13,7 +13,73 @@ class SeatPage extends StatefulWidget{
       appBar: AppBar(
         title: const Text('좌석 선택');
       ),
-      
+      body: Column(
+        children: [
+          SeatLayOut(), //seat layouts to click on
+          SelectionButton(), //bottom most purple
+        ],
+      )
     )
   }
 }
+
+class SeatLayOut extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    child: Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [Text('A'), Text('B'), Text(""), Text("C"), Text("D")],)
+          for(int i = 1; i<21; i++){
+            SeatRow(i);
+          },
+      ],
+    )
+  }
+
+  Padding SeatRow(int i) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        SeatBox(),
+        SeatBox(),
+        Expanded(
+          child: Center(
+            child: Text(
+              '$i',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+              ),
+
+          )
+        ),
+        SeatBox(),
+        SeatBox(),
+      ),
+    )
+  }
+
+  Widget SeatBox() {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 2),
+        child: AspectRatio(
+          aspectRatio: 1,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
+
